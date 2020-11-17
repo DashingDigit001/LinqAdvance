b2557b51fb87c037c3fd822769e5b57e008c2675
Hank <dashing.hank@gmail.com>
2020-11-17 16:17:44 +0800

    init

---
+++ Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Linq;
+
+namespace LinqTest
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Sample_Take_Lambda();
+            Sample_TakeWhile_Lambda();
+            Sample_Skip_Lambda();
+            Sample_SkipWhile_Lambda();
+            Sample_Join_Lambda();
+            Sample_OrderBy_Lambda_Numbers();
+            Sample_OrderByDescending_Lambda();
+        }
+
+        private static void Sample_Take_Lambda()
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            var result = numbers.Take(5);
+
+            Console.WriteLine("Takes the first 5 numbers only:");
+            foreach (int number in result)
+                Console.WriteLine(number);
+        }
+
+        private static void Sample_TakeWhile_Lambda()
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            var result = numbers.TakeWhile(n => n<3);
+
+            Console.WriteLine("Takes numbers one by one, and stops when condition is no longer met:");
+            foreach (int number in result)
+                Console.WriteLine(number);
+        }
+        private static void Sample_Skip_Lambda()
+        {
+            string[] words = { "one", "two", "three", "four", "five", "six" };
+
+            var result = words.Skip(4);
+
+            Console.WriteLine("Skips the first 4 words:");
+            foreach (string word in result)
+                Console.WriteLine(word);
+        }
+
+        private static void Sample_SkipWhile_Lambda()
+        {
+            string[] words = { "one", "two", "three", "four", "five", "six" };
+
+            var result = words.SkipWhile(w => w.Length == 3);
+
+            Console.WriteLine("Skips words while the condition is met:");
+            foreach (string word in result)
+                Console.WriteLine(word);
+        }
+
+
+        private static void Sample_Join_Lambda()
+        {
+            string[] warmCountries = { "Turkey", "Italy", "Spain", "Saudi Arabia", "Etiobia" };
+            string[] europeanCountries = { "Denmark", "Germany", "Italy", "Saudi Arabia", "Portugal", "Spain" };
+
+            var result = warmCountries.Join(europeanCountries, d => d, e => e, (d, e) => d);
+
+            Console.WriteLine("Joined countries which are both warm and Europan:");
+            foreach (var country in result) // Note: result is an anomymous type, thus must use a var to iterate.
+                Console.WriteLine(country);
+        }
+
+        private static void Sample_OrderBy_Lambda_Numbers()
+        {
+            int[] numbers = { 7, 9, 5 };
+
+            var result = numbers.OrderBy(n => n);
+
+            Console.WriteLine("Ordered list of numbers:");
+            foreach (int number in result)
+                Console.WriteLine(number);
+        }
+
+        private static void Sample_OrderByDescending_Lambda()
+        {
+            string[] names = { "Ned", "Ben", "Susan" };
+
+            var result = names.OrderByDescending(n => n);
+
+            Console.WriteLine("Descending ordered list of names:");
+            foreach (string name in result)
+                Console.WriteLine(name);
+        }
+    }
+}
